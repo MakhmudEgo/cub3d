2859a430ee17bf98bf2f6d45efb70745c440a8cd
Makhmud Ego <makhmudego@yandex.ru>
2020-09-20 18:56:00 +0300

    norm create_map.c

--- create_map.c
+++ create_map.c
@@ -12,42 +12,51 @@
 
 #include "cub3d.h"
 
-static void map_fill(char *s, int current, int last, t_data *data)
+static int	is_valid_map(t_data *data, int n, int current, int last)
 {
-	int		n;
 	int		s_bf;
 	int		s_af;
+	char	*s;
+
+	s_bf = current != 0 ? (int)ft_strlen(data->map[current - 1]) : 0;
+	s_af = current != last ? (int)ft_strlen(data->map[current + 1]) : 0;
+	s = data->map[current];
+	return (((s[n] == '0' || s[n] == 'N') &&
+	(current == 0 || current == last
+	|| s_bf <= n || s_af <= n
+	|| (n != 0 && s[n - 1] == ' ') || s[n + 1] == ' '
+	|| s[n + 1] == '\0' || n == 0
+	|| (current < last && data->map[current + 1][n] == ' ')
+	|| (s_bf >= n && data->map[current - 1][n] == ' ')))
+	|| !ft_strchr("12 0NWES", s[n])
+	|| (current != last && data->map[current + 1][0] == '\0')
+	|| data->plyr > 1);
+}
+
+static void	map_fill(char *s, int current, int last, t_data *data)
+{
+	int		n;
 
 	n = 0;
-    s_bf = current != 0 ? ft_strlen(data->map[current - 1]) : 0;
-    s_af = current != last ? ft_strlen(data->map[current + 1]) : 0;
-    while (s[n])
-    {
+	while (s[n])
+	{
 		if (ft_strchr("NWES", s[n]))
 		{
 			data->plyr++;
 			data->strt_x = n * SZ_PX + (SZ_PX / 2);
 			data->strt_y = current * SZ_PX + (SZ_PX / 2);
 		}
-		if (((s[n] == '0' || s[n] == 'N') &&
-            (current == 0 || current == last
-            || s_bf <= n || s_af <= n
-            || (n != 0 && s[n - 1] == ' ') || s[n + 1] == ' '
-            || s[n + 1] == '\0' || n == 0
-            || (current < last && data->map[current + 1][n] == ' ')
-            || (s_bf >= n && data->map[current - 1][n] == ' ')))
-            || !ft_strchr("12 0NWES", s[n])
-            || (current != last && data->map[current + 1][0] == '\0')
-            || data->plyr > 1
-            )
+		if (is_valid_map(data, n, current, last))
 			exit_notify("No valid map\n", 66);
 		if (s[n] == '2')
-			sp_lstadd_back(&data->sprts, sp_lstnew(n * SZ_PX + (SZ_PX / 2), current * SZ_PX + (SZ_PX / 2), 0));
-	    n++;
-    }
+			sp_lstadd_back(&data->sprts, sp_lstnew(n * SZ_PX + (SZ_PX / 2),
+			current * SZ_PX + (SZ_PX / 2), 0));
+		n++;
+	}
 }
 
-void create_map(t_list *t_map, t_data *data) {
+void		create_map(t_list *t_map, t_data *data)
+{
 	int map_size;
 	int i;
 
@@ -55,7 +64,6 @@ void create_map(t_list *t_map, t_data *data) {
 	data->map = malloc(sizeof(char *) * map_size + 1);
 	data->map[map_size] = 0x0;
 	i = 0;
-
 	while (t_map)
 	{
 		if (t_map->content)
